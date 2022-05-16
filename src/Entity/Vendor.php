3f9032466fb95bd9ac98933415209de40ac8ce56
VikingDDM <chrisdulin4ever@gmail.com>
2022-05-16 11:08:08 +0200

    Make registered in services.xml AdminMainMenuListener and translations for it

--- src/Entity/Vendor.php
+++ src/Entity/Vendor.php
@@ -1,30 +1,11 @@
 <?php
 
-/*
- * This file has been created by developers from BitBag.
- * Feel free to contact us once you face any issues or want to start
- * You can find more information about us on https://bitbag.io and write us
- * an email on hello@bitbag.io.
- */
-
 declare(strict_types=1);
 
 namespace BitBag\SyliusMultiVendorMarketplacePlugin\Entity;
 
 class Vendor implements VendorInterface
 {
-    private int $id;
-
-    private Customer $customer;
-
-    private ?string $companyName;
-
-    private ?string $taxIdentifier;
-
-    private ?string $phoneNumber;
-
-    private ?VendorAddress $vendorAddress;
-
     public function getId(): int
     {
         return $this->id;
@@ -75,13 +56,13 @@ class Vendor implements VendorInterface
         $this->vendorAddress = $vendorAddress;
     }
 
-    public function getCustomer(): Customer
-    {
-        return $this->customer;
-    }
+    private int $id;
 
-    public function setCustomer(Customer $customer): void
-    {
-        $this->customer = $customer;
-    }
+    private ?string $companyName;
+
+    private ?string $taxIdentifier;
+
+    private ?string $phoneNumber;
+
+    private ?VendorAddress $vendorAddress;
 }
--- src/Entity/VendorInterface.php
+++ src/Entity/VendorInterface.php
@@ -1,19 +1,10 @@
 <?php
 
-/*
- * This file has been created by developers from BitBag.
- * Feel free to contact us once you face any issues or want to start
- * You can find more information about us on https://bitbag.io and write us
- * an email on hello@bitbag.io.
- */
-
 declare(strict_types=1);
 
 namespace BitBag\SyliusMultiVendorMarketplacePlugin\Entity;
 
-use Sylius\Component\Resource\Model\ResourceInterface;
-
-interface VendorInterface extends ResourceInterface
+interface VendorInterface
 {
     public function getId(): int;
 
@@ -34,8 +25,4 @@ interface VendorInterface extends ResourceInterface
     public function getVendorAddress(): ?VendorAddress;
 
     public function setVendorAddress(?VendorAddress $vendorAddress): void;
-
-    public function getCustomer(): Customer;
-
-    public function setCustomer(Customer $customer): void;
 }
