07689e088dfadcb0a926105048b6d696fc0aae6e
VikingDDM <chrisdulin4ever@gmail.com>
2022-05-26 13:16:59 +0200

    Create blocked property in Vendor entity for the state machine, make state machine needed configuration and custom action for blocking vendor, add messages and flashes translations

---
+++ src/Entity/Vendor.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusMultiVendorMarketplacePlugin\Entity;
+
+class Vendor implements VendorInterface
+{
+    public const STATUS_UNVERIFIED = 'unverified';
+
+    public const STATUS_VERIFIED = 'verified';
+
+    public const BLOCKED = 'blocked';
+
+    public const UNBLOCKED = 'unblocked';
+
+    private ?int $id;
+
+    private Customer $customer;
+
+    private ?string $companyName;
+
+    private ?string $taxIdentifier;
+
+    private ?string $phoneNumber;
+
+    private ?VendorAddress $vendorAddress;
+
+    private string $status = self::STATUS_UNVERIFIED;
+
+    private string $blocked = self::UNBLOCKED;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getCompanyName(): ?string
+    {
+        return $this->companyName;
+    }
+
+    public function setCompanyName(?string $companyName): void
+    {
+        $this->companyName = $companyName;
+    }
+
+    public function getTaxIdentifier(): ?string
+    {
+        return $this->taxIdentifier;
+    }
+
+    public function setTaxIdentifier(?string $taxIdentifier): void
+    {
+        $this->taxIdentifier = $taxIdentifier;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(?string $phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    public function getVendorAddress(): ?VendorAddress
+    {
+        return $this->vendorAddress;
+    }
+
+    public function setVendorAddress(?VendorAddress $vendorAddress): void
+    {
+        $this->vendorAddress = $vendorAddress;
+    }
+
+    public function getCustomer(): Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(Customer $customer): void
+    {
+        $this->customer = $customer;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function getBlocked(): string
+    {
+        return $this->blocked;
+    }
+
+    public function setBlocked(string $blocked): void
+    {
+        $this->blocked = $blocked;
+    }
+
+
+}
