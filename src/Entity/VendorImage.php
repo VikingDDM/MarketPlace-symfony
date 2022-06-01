1c6e35c967fb45ef6942d731dea72229b4f4a79d
VikingDDM <chrisdulin4ever@gmail.com>
2022-06-01 13:08:48 +0200

    Vendor page layout.

---
+++ src/Entity/VendorImage.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusMultiVendorMarketplacePlugin\Entity;
+
+final class VendorImage implements VendorImageInterface
+{
+    private ?int $id;
+
+    private ?string $path;
+
+    private ?VendorInterface $vendor;
+
+    public function getId(): int
+    {
+        return $this->getId();
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(?string $path): void
+    {
+        $this->path = $path;
+    }
+
+    public function getVendor(): VendorInterface
+    {
+        return $this->vendor;
+    }
+
+    public function setVendor(VendorInterface $vendor): void
+    {
+        $this->vendor = $vendor;
+    }
+}
