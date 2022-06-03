38b49e85305a46e3ba158faba102cb2e97018d5c
VikingDDM <chrisdulin4ever@gmail.com>
2022-06-03 15:32:36 +0200

    Create vendor edit page skeleton

--- src/Entity/VendorInterface.php
+++ src/Entity/VendorInterface.php
@@ -1,39 +1,49 @@
 <?php
+
 /*
  * This file has been created by developers from BitBag.
  * Feel free to contact us once you face any issues or want to start
  * You can find more information about us on https://bitbag.io and write us
  * an email on hello@bitbag.io.
  */
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusMultiVendorMarketplacePlugin\Entity;
 
-use Doctrine\Common\Collections\Collection;
-use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Resource\Model\ResourceInterface;
 
-interface VendorInterface extends VendorDataInterface
+interface VendorInterface extends ResourceInterface
 {
-    public function getCustomer(): Customer;
+    public function getId(): ?int;
+
+    public function setId(?int $id): void;
+
+    public function getCompanyName(): ?string;
+
+    public function setCompanyName(?string $companyName): void;
+
+    public function getTaxIdentifier(): ?string;
+
+    public function setTaxIdentifier(?string $taxIdentifier): void;
+
+    public function getPhoneNumber(): ?string;
+
+    public function setPhoneNumber(?string $phoneNumber): void;
+
+    public function getVendorAddress(): ?VendorAddressInterface;
+
+    public function setVendorAddress(?VendorAddressInterface $vendorAddress): void;
+
+    public function getCustomer(): CustomerInterface;
 
     public function setCustomer(Customer $customer): void;
 
-    public function getSlug(): ?string;
+    public function getStatus(): string;
 
-    public function setSlug(?string $slug): void;
+    public function setStatus(string $status): void;
 
-    public function getDescription(): ?string;
+    public function getBlocked(): string;
 
-    public function setDescription(?string $description): void;
-
-    public function getImage(): ?VendorImageInterface;
-
-    public function setImage(VendorImageInterface $vendorImage): void;
-
-    /** @return Collection<int, VendorImageInterface> */
-    public function getProducts(): Collection;
-
-    public function addProduct(ProductInterface $product): void;
-
-    public function removeProduct(ProductInterface $product): void;
+    public function setBlocked(string $blocked): void;
 }
