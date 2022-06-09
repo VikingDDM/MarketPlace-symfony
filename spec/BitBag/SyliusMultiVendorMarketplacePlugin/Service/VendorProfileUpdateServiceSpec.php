9a102ada5189a2bf5e92e65b7ccd8dbcbab34aea
VikingDDM <chrisdulin4ever@gmail.com>
2022-06-09 14:04:38 +0200

    Fixed phpstan

---
+++ spec/BitBag/SyliusMultiVendorMarketplacePlugin/Service/VendorProfileUpdateServiceSpec.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace spec\BitBag\SyliusMultiVendorMarketplacePlugin\Service;
+
+use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\VendorInterface;
+use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\VendorProfileInterface;
+use BitBag\SyliusMultiVendorMarketplacePlugin\Service\RemoverInterface;
+use BitBag\SyliusMultiVendorMarketplacePlugin\Service\VendorProfileUpdateService;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Mailer\Sender\SenderInterface;
+
+class VendorProfileUpdateServiceSpec extends ObjectBehavior
+{
+    function let(EntityManagerInterface $entityManager,
+                 SenderInterface $sender,
+                 RemoverInterface $remover)
+    {
+        $this->beConstructedWith($entityManager, $sender, $remover);
+    }
+    
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(VendorProfileUpdateService::class);
+    }    
+    function it_gets_correct_data(     
+        VendorInterface $vendor,
+        VendorProfileInterface $vendorData,
+        EntityManagerInterface $manager
+    )
+    {
+        $this->entityManager->flush()->shouldBeCalled(1);
+        $vendor->getCompanyName()->shouldBeCalled(1);
+        $vendor->getTaxIdentifier()->shouldBeCalled(1);
+        $vendor->getPhoneNumber()->shouldBeCalled(1);
+        $vendor->getVendorAddress()->shouldBeCalled(4);
+
+        $this->setVendorFromData($vendorData, $vendor);
+    }
+}
