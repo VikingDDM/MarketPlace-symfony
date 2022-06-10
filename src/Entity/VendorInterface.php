322bf3c1204f5ef1ddec3cbbd105f6560b79d14e
VikingDDM <chrisdulin4ever@gmail.com>
2022-06-10 07:01:46 +0200

    Changed entity to mapped-superclass

--- src/Entity/VendorInterface.php
+++ src/Entity/VendorInterface.php
@@ -1,24 +1,16 @@
 <?php
-
 /*
  * This file has been created by developers from BitBag.
  * Feel free to contact us once you face any issues or want to start
  * You can find more information about us on https://bitbag.io and write us
  * an email on hello@bitbag.io.
  */
-
 declare(strict_types=1);
 
 namespace BitBag\SyliusMultiVendorMarketplacePlugin\Entity;
 
-use Sylius\Component\Resource\Model\ResourceInterface;
-
-interface VendorInterface extends ResourceInterface
+interface VendorInterface extends VendorProfileInterface
 {
-    public const STATUS_UNVERIFIED = 'unverified';
-
-    public const STATUS_VERIFIED = 'verified';
-
     public function getId(): ?int;
 
     public function setId(?int $id): void;
@@ -39,11 +31,7 @@ interface VendorInterface extends ResourceInterface
 
     public function setVendorAddress(?VendorAddressInterface $vendorAddress): void;
 
-    public function getCustomer(): CustomerInterface;
+    public function getCustomer(): ?CustomerInterface;
 
-    public function setCustomer(Customer $customer): void;
-
-    public function getStatus(): string;
-
-    public function setStatus(string $status): void;
+    public function setCustomer(CustomerInterface $customer): void;
 }
