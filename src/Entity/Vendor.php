c2cd40ca9dd47767e3ec2e23bbdcbbb0e2143fa6
VikingDDM <chrisdulin4ever@gmail.com>
2022-06-10 08:29:51 +0200

    Code review changes

--- src/Entity/Vendor.php
+++ src/Entity/Vendor.php
@@ -11,21 +11,31 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusMultiVendorMarketplacePlugin\Entity;
 
-use Sylius\Component\Resource\Model\ResourceInterface;
-
-class Vendor implements VendorProfileInterface, VendorInterface, ResourceInterface
+class Vendor implements VendorInterface
 {
-    protected ?int $id;
+    public const STATUS_UNVERIFIED = 'unverified';
 
-    protected CustomerInterface $customer;
+    public const STATUS_VERIFIED = 'verified';
 
-    protected ?string $companyName;
+    public const BLOCKED = 'blocked';
 
-    protected ?string $taxIdentifier;
+    public const UNBLOCKED = 'unblocked';
 
-    protected ?string $phoneNumber;
+    private ?int $id;
 
-    protected ?VendorAddressInterface $vendorAddress;
+    private CustomerInterface $customer;
+
+    private ?string $companyName;
+
+    private ?string $taxIdentifier;
+
+    private ?string $phoneNumber;
+
+    private ?VendorAddressInterface $vendorAddress;
+
+    private string $status = self::STATUS_UNVERIFIED;
+
+    private string $blocked = self::UNBLOCKED;
 
     public function getId(): ?int
     {
@@ -85,5 +95,25 @@ class Vendor implements VendorProfileInterface, VendorInterface, ResourceInterfa
     public function setCustomer(CustomerInterface $customer): void
     {
         $this->customer = $customer;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function getBlocked(): string
+    {
+        return $this->blocked;
+    }
+
+    public function setBlocked(string $blocked): void
+    {
+        $this->blocked = $blocked;
     }
 }
